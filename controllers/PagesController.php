e69cc54a8e2af5288252a25cbfad8799fd9f9b18
fil12 <filimonv12@gmail.com>
2017-03-09 14:04:38 +0200

    views

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -3,7 +3,21 @@
      class PagesController extends Controller{
 
          public function index (){
-             echo 'Nakonec to';
+             $this->data['test_content'] =  'Nakonec to';
+         }
+
+
+         public function view(){
+             $params = App::getRouter()->getParams();
+
+             if (isset($params[0])){
+                 $alias = strtolower($params[0]);
+
+                 $this->data['content'] = "Here will be a page with '{$alias}' alias ";
+
+
+
+             }
          }
 
 
--- lib/app.class.php
+++ lib/app.class.php
@@ -26,8 +26,20 @@ class App
         $conrtoller_object = new $controller_class();
 
         if (method_exists($conrtoller_object,$conrtoller_method)){
-            $result = $conrtoller_object->$conrtoller_method();
+            //Controller's action may return a view path
+
+            $view_path = $conrtoller_object->$conrtoller_method();
+            $view_object = new View($conrtoller_object->getData(), $view_path);
+            $content = $view_object->render();
+
+        }else{
+            throw new Exception('Method '.$conrtoller_method.' of class '.$controller_class.' does not exist.');
         }
+
+        $layout = self::$router->getRoute();
+        $layout_path = VIEWS_PATH.DS.$layout.'.html';
+        $layout_view_object = new View ( compact('content'), $layout_path);
+        echo $layout_view_object->render();
 
 
 
